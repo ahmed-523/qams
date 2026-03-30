ed044f5b5e02b4e6a798e9fabde075aa5f53b061
Ahmed <mubasharahmed523@gmail.com>
2026-03-30 11:21:52 +0500

    force https in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,9 +20,11 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
-        Schema::defaultStringLength(191);
+{
+    if (config('app.env') === 'production') {
+        \URL::forceScheme('https');
     }
+}
 
     
 }
